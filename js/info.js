c06cf7ee362efa2cc260a6ec2eb16b3166bdb03a
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2017-09-06 17:05:53 +0300

    Created a development branch for further updates and refactoring. I hope to migrate to a newer technology for this page, but mdl isn't really helping

--- js/info.js
+++ js/info.js
@@ -2,7 +2,7 @@ var aboutData = {
     description: "<br><br>Hi, I'm Viorel!<br>I'm a student at the University of Bucharest, preparing to become an engineer.<br>I'm also a volunteer in the Student's Association at Mathematics and Computer Science.<br><br>In my free time I play drums and study about science and human psychology.",
 
     contactInfoVars: ["Phone", "E-mail", "Employment"],
-    contactInfoData: ["0722 509 867", "alex.viorel.117@gmail.com", "Student/Freelancer"]
+    contactInfoData: ["0722 509 867", "alex.viorel.117@gmail.com", "Student - Junior Developer"]
 };
 
 var skills = [{
@@ -22,27 +22,31 @@ var skills = [{
 
     {
         title: "Development Tools",
-        description: "I use an <b>Ubuntu</b> distro as an alternative to Windows. I'm still a n00b when it comes to shell scripts, but very enthusiastic to learning more.<br><br>I utilize <b>Git</b> for Version Control, as my projects are on GitHub and BitBucket."
+        description: "I use an <b>Ubuntu</b> distro as an alternative to Windows. I am still a beginner, but also very enthusiastic to learning more.<br><br>I utilize <b>Git</b> for Version Control, as my projects are on GitHub and BitBucket."
     }
 ];
 
 var experience = [{
         title: "Part-Time Junior Developer at Avitech Romania",
-        description: "August 2017 - Present<br><br>Develop according to requirements and serve top-quality software solutions."
+        period: "August 2017 - Present",
+        description: "Develop according to requirements and serve top-quality software solutions."
     },
 
     {
         title: "Angular Trainee at Avitech Romania",
-        description: "June 2017 - August 2017<br><br>Training in using Angular for web applications."
+        period: "June 2017 - August 2017"
+        description: "Training in using Angular for web applications."
     },
 
     {
         title: "Part-Time QA at Cornerstone Technologies",
-        description: "March 2016 - August 2016<br><br>Test applications to discover bugs before pushing a new build into production.<br>After this step, ensure highest level of quality per product."
+        period: "March 2016 - August 2016",
+        description: "Test applications to discover bugs before pushing a new build into production.<br>After this step, ensure highest level of quality per product."
     },
 
     {
         title: "Volunteer in AS-MI",
-        description: "November 2014 - Present<br><br>As a volunteer, I have accomplished a great number of tasks: from preparing a stage for presentations or shows to promoting a national contest throughout numerous universities from Romania.<br><br>A few projects I've volunteered for are:<ul id=\"asmi-projects-list\"><li><b>Cariere v4.0</b> at which I've managed presentations during the event.</li><li><b>S.T.A.R.T. 2015</b> - Logistics during trainings.</li><li>Promoting <b>imSMART 2015</b> at a national level in numerous universities.</li><li>Organizing the Music Night and logistics during <b>Artă ‘n Dar</b>, which is a charity event.</li><li>Inform pupils at <b>The University of Bucharest Educational Fair</b> of what our faculty has to offer them.</li><li><b>Promoting the faculty in high schools</b> so pupils would know what education they may receive as a student there.</li><li><b>Numerous laboratories</b> at which I've presented topics on basic C or Java programming.</li><li><b>Official photographer</b> at Cariere V6.0.</li></ul>"
+        period: "November 2014 - Present",
+        description: "As a volunteer, I have accomplished a great number of tasks: from preparing a stage for presentations or shows to promoting a national contest throughout numerous universities from Romania.<br><br>A few projects I've volunteered for are:<ul id=\"asmi-projects-list\"><li><b>Cariere v4.0</b> at which I've managed presentations during the event.</li><li><b>S.T.A.R.T. 2015</b> - Logistics during trainings.</li><li>Promoting <b>imSMART 2015</b> at a national level in numerous universities.</li><li>Organizing the Music Night and logistics during <b>Artă ‘n Dar</b>, which is a charity event.</li><li>Inform pupils at <b>The University of Bucharest Educational Fair</b> of what our faculty has to offer them.</li><li><b>Promoting the faculty in high schools</b> so pupils would know what education they may receive as a student there.</li><li><b>Numerous laboratories</b> at which I've presented topics on basic C or Java programming.</li><li><b>Official photographer</b> at Cariere V6.0.</li></ul>"
     }
 ];
